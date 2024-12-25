2239354d63b1524352e81abcd7ad1c1e11c6bd17
MarinCervinschi <marincervinschi3@gmail.com>
2024-12-25 15:46:47 +0100

    day_25 visualisation

--- day_25/main_25.py
+++ day_25/main_25.py
@@ -1,11 +1,11 @@
+import os, time
+from colorama import Fore, Style
+
 if __name__ == "__main__":
 
     with open("puzzle.txt") as f:
-        locks = []
-        keys = []
-
+        locks, keys = [], []
         lock, key = [], []
-
         for row in f:
             row = row.strip()
             if row and (row == '#####' or lock) and not key:
@@ -20,43 +20,59 @@ if __name__ == "__main__":
                     keys.append(key)
                     key = []
 
-
-        def _print(m, c):
-            for row in m:
-                print(''.join(row))
-
-            print(f"{3 * '-'}{c}{3 * '-'}")
-
         locks_heights = [[-1] * 5 for _ in range(len(locks))]
         keys_heights = [[-1] * 5 for _ in range(len(keys))]
 
         for i, lock in enumerate(locks):
-            _print(lock, '🔒')
             for r in range(7):
                 for c in range(5):
                     if lock[r][c] == '#':
                         locks_heights[i][c] += 1 
             
-            print(locks_heights[i])
-            print(8 * '-')
-
-            
         for i, key in enumerate(keys):
-            _print(key, '🔑')
             for r in range(7):
                 for c in range(5):
                     if key[r][c] == '#':
                         keys_heights[i][c] += 1 
-            print(keys_heights[i])
-            print(8 * '-')
 
+        def visulisation(lock, key, match=False):
 
+            def _print(m, c):
+                if c == '🔒':
+                    color = Fore.RED
+                elif c == '🔑':
+                    color = Fore.YELLOW
+                else:
+                    color = Fore.GREEN
+
+                for row in m:
+                    row = [color + char + Style.RESET_ALL if char == '#' else char for char in row]
+                    print(''.join(row))
+
+                print(f"{3 * '-'}{c}{3 * '-'}")
+
+            os.system('clear')
+            _print(lock, '🔒')
+            _print(key, '🔑')
+            print(f"Total match: {fit}")
+            if match:
+                time.sleep(0.1)
+                match_matrix = [[c[0] if c[0] == '#' else c[1] for c in zip(*r)] for r in zip(lock, key)]
+                os.system('clear')
+                _print(match_matrix, '🔐')
+                print(f"Total match: {fit}")
+                time.sleep(1)
+            time.sleep(0.01)
+            
         fit = 0
-        for lock_height in locks_heights:
-            for key_height in keys_heights:
+        for i, lock_height in enumerate(locks_heights):
+            for j, key_height in enumerate(keys_heights):
+                match = False
                 try_fit = [a + b for a, b in zip(lock_height, key_height)]
                 if all(t < 6 for t in try_fit):
                     fit += 1
+                    match = True
+                visulisation(locks[i], keys[j], match)
         
         print(f"The number of unique lock/key pairs that fit together without overlapping in any column is {fit}")
 
