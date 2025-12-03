e9c02312fb7400ffce686d6a70dfbd261e24906e
MarinCervinschi <marincervinschi3@gmail.com>
2025-12-03 06:44:25 +0100

    2025-day03: clean

--- 2025/day_03/main_03.py
+++ 2025/day_03/main_03.py
@@ -1,33 +1,14 @@
-def part_1(nums):
-    ans = 0
-    first = last = 0
-    for num in nums:
-        temp = max(num)
-        i = num.index(temp)
-        if i == len(num) - 1:
-            last = temp
-            first = max(num[:-1])
-        else:
-            first = temp
-            last = max(num[i + 1 :])
-
-        ans += int(str(first) + str(last))
-
-    print(ans)
-
-
-def part_2(nums):
+def solve(nums, size):
     ans = 0
     for num in nums:
         stack = []
         for i, d in enumerate(num):
-
             left = len(num) - i
 
-            while stack and d > stack[-1] and len(stack) + left > 12:
+            while stack and d > stack[-1] and len(stack) + left > size:
                 stack.pop()
 
-            if len(stack) < 12:
+            if len(stack) < size:
                 stack.append(d)
 
         temp = "".join(stack)
@@ -44,5 +25,5 @@ if __name__ == "__main__":
 
     with open(PATH / "puzzle.txt", "r") as file:
         nums = re.findall(r"\d+", file.read())
-        part_1(nums)
-        part_2(nums)
+        solve(nums, size=2)
+        solve(nums, size=12)
