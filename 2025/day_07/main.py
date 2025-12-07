1e6918e262579901115558baa34017d00e325a67
MarinCervinschi <marincervinschi3@gmail.com>
2025-12-07 11:33:43 +0100

    2025-day07: dp...

--- 2025/day_07/main.py
+++ 2025/day_07/main.py
@@ -1,47 +1,26 @@
-from re import split
+from collections import Counter
 
 
-def part_1(grid, start):
+def solve(grid, start_col):
 
-    up = (-1, 0)
-    current_positions = [start]
+    dp = Counter()
+
+    dp[start_col] = 1
     splits = 0
-    rows, cols = len(grid), len(grid[0])
-    for r in range(1, rows):
-        for c in range(cols):
-            nx, ny = r + up[0], c + up[1]
+    for r in range(1, len(grid)):
+        next_dp = Counter()
+        for c, count in dp.items():
+            if grid[r][c] == "^":
+                splits += 1
 
-            if 0 <= nx < rows and 0 <= ny < cols:
-                if (nx, ny) in current_positions:
-                    if grid[r][c] == "^":
-                        splits += 1
-                        grid[r][c - 1] = "|"
-                        grid[r][c + 1] = "|"
-                        current_positions.append((r, c + 1))
-                        current_positions.append((r, c - 1))
-                    else:
-                        grid[r][c] = "|"
-                        current_positions.append((r, c))
+                next_dp[c - 1] += count
+                next_dp[c + 1] += count
+            else:
+                next_dp[c] += count
+        dp = next_dp
+
     print(splits)
-    return grid
-
-
-def part_2(grid, start_col):
-    rows, cols = len(grid), len(grid[0])
-
-    dp = [[0] * cols for _ in range(rows)]
-    dp[1][start_col] = 1
-    for r in range(1, rows - 1):
-        for c in range(cols):
-            if dp[r][c] > 0:
-                if grid[r][c] == "|":
-                    dp[r + 1][c] += dp[r][c]
-                elif grid[r][c] == "^":
-                    dp[r + 1][c + 1] += dp[r][c]
-                    dp[r + 1][c - 1] += dp[r][c]
-
-    end_paths = [i for i, c in enumerate(grid[-1]) if c == "|"]
-    print(sum(dp[-1][c] for c in end_paths))
+    print(dp.total())
 
 
 def get_data(file_path):
@@ -56,6 +35,5 @@ if __name__ == "__main__":
     PATH = Path(__file__).parent
 
     data = get_data(PATH / "puzzle.txt")
-    start = data[0].index("S")
-    grid = part_1(grid=data, start=(0, start))
-    part_2(grid=grid, start_col=start)
+    start_col = data[0].index("S")
+    solve(grid=data, start_col=start_col)
